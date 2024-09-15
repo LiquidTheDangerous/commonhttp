8388d376d928df6eed930d6fa7f43692d15ad7d0
Andrey Semenov <semenov.ao@greendatasoft.ru>
2024-09-15 05:19:58 +0500

    upd example

--- example/controller/http/middleware/middleware.go
+++ example/controller/http/middleware/middleware.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
+	"time"
 
 	"github.com/LiquidTheDangerous/commonhttp/controller"
 	"github.com/LiquidTheDangerous/commonhttp/middleware"
@@ -13,15 +14,23 @@ type ExampleControllerWithMiddleware struct {
 }
 
 func (e *ExampleControllerWithMiddleware) Routes() controller.Routes {
-	return controller.Routes{controller.Route("GET", "/api/hello", e.SayHello)}
+	return controller.Routes{
+		controller.Route("GET", "/api/hello", e.SayHello),
+		controller.Route("POST", "/api/hi", e.SayHi),
+	}
 }
 
 func (e *ExampleControllerWithMiddleware) SayHello(w http.ResponseWriter, _ *http.Request) {
-	_, _ = io.WriteString(w, "Hello World")
-	panic("whatever")
+	_, _ = io.WriteString(w, "Hello")
+	panic("Hello")
+}
+
+func (e *ExampleControllerWithMiddleware) SayHi(w http.ResponseWriter, _ *http.Request) {
+	_, _ = io.WriteString(w, "Hi")
 }
 
 func PanicHandlingMiddleware(w http.ResponseWriter, r *http.Request, next http.Handler) {
+	slog.Log(r.Context(), slog.LevelInfo, "(PanicHandlingMiddleware) handling panic middleware applied")
 	defer func() {
 		v := recover()
 		if v == nil {
@@ -34,14 +43,18 @@ func PanicHandlingMiddleware(w http.ResponseWriter, r *http.Request, next http.H
 		case string:
 			errstring = u
 		}
-		slog.Warn("panic recovered", slog.String("error", errstring))
+		slog.Warn("(PanicHandlingMiddleware) panic recovered", slog.String("error", errstring))
 	}()
 	next.ServeHTTP(w, r)
+	slog.Info("(PanicHandlingMiddleware) end")
 }
 
 func LoggingMiddlewareFunc(w http.ResponseWriter, r *http.Request, next http.Handler) {
-	slog.Log(r.Context(), slog.LevelInfo, "handling request for: "+r.RequestURI)
+	startTime := time.Now()
+	slog.Log(r.Context(), slog.LevelInfo, "(LoggingMiddleware) handling request for: "+r.RequestURI)
 	next.ServeHTTP(w, r)
+	d := time.Since(startTime)
+	slog.Log(r.Context(), slog.LevelInfo, "(LoggingMiddleware) end request for: "+r.RequestURI+" elapsed: "+d.String())
 }
 
 func main() {
@@ -50,6 +63,8 @@ func main() {
 		&ExampleControllerWithMiddleware{},
 		controller.WithMiddlewares(
 			middleware.MiddlewareFunc(LoggingMiddlewareFunc),
+		),
+		controller.WithMiddlewares(
 			middleware.MiddlewareFunc(PanicHandlingMiddleware),
 		),
 	)
